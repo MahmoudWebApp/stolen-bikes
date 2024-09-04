35aeebc642d4fd2630b3673137927f6e87d98acd
unknown <mahmoud.ahmad@nasca-sy.com>
2024-09-05 00:40:47 +0300

    fix

--- src/stolen-bikes/StolenBikes.tsx
+++ src/stolen-bikes/StolenBikes.tsx
@@ -2,7 +2,7 @@ import { IBike, IFilter } from "@/models/bikes.model";
 import { useGetStolenBikesQuery } from "@/services/bikesApi";
 import { useState } from "react";
 import BikeCard from "./BikeCard";
-import BikePagination from "./BikePagination";
+// import BikePagination from "./BikePagination";
 import Loader from "./Loader";
 import SearchTitle from "./SearchTitle";
 // import DateRangeSearch from "./DateRangeSearch";
@@ -55,13 +55,13 @@ const StolenBikes: React.FC = () => {
                                 dateStolenReport={`${b.stolen_coordinates}`}
                                 location={b.stolen_location} />)}
                         </div>
-                        {filter.title !== '' ? <></> :
+                        {/* {filter.title !== '' ? <></> :
                             <BikePagination
                                 pageSize={filter.per_page} currentPage={filter.page}
                                 setPage={(page: number) => setFilter(prevState => {
                                     return { ...prevState, page: page };
                                 })} />
-                        }
+                        } */}
 
                     </div>) : (
                     <div className="empty text-center ">
